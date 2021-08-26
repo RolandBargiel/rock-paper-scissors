7693fd0962e7e9ac37591b77025bf7a78ed85d62
RolandBargiel <bargielroland@gmail.com>
2021-08-26 14:14:24 -0600

    Final Non-GUI Version of Game

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 //This function randomly selects the computer's choice of rock, paper, or scissor
 
 function computerPlay() {
-    const randomNumber = (Math.floor(Math.random() * 3));
+    let randomNumber = (Math.floor(Math.random() * 3));
     
     if (randomNumber == 0) {
         return "rock";
@@ -23,7 +23,8 @@ function userPlay() {
         return userSelection;
     }
     else {
-        return "Valid response required - please enter rock, paper, or scissors";
+        alert("Nice try - the only valid weapons here are rock, paper, or scissors");
+        return;
     }
 }
 
@@ -35,23 +36,19 @@ function playRound() {
     let computer_play = computerPlay();
     console.log("user_play = " + user_play);
     console.log("computer_play = " + computer_play);
-
-    let user_score = 0;
-    let computer_score = 0;
-
     
+        
     if (user_play == computer_play) {
         return "It's a tie!";
     }
     else if (user_play == "rock" && computer_play == "paper") {
-        computer_score += 1;
         return "You lose! Paper beats rock!";
     }
     else if (user_play == "rock" && computer_play == "scissors") {
         return "You win! Rock beats scissors!";
     }
     else if (user_play == "paper" && computer_play == "scissors") {
-        return "You lose! Scissors beats papers!";
+        return "You lose! Scissors beats paper!";
     }
     else if (user_play == "paper" && computer_play == "rock") {
         return "You win! Paper beats rock!";
@@ -63,14 +60,23 @@ function playRound() {
         return "You lose! Rock beats scissors!";
     }
     else {
-        return "What the heck just happened? I don't know!";
+        return "What the heck just happened? Oopsie!";
     }
 }
 
-console.log(playRound())
+
+
 
 function game() {
+    
+   console.log(playRound())
+   console.log(playRound())
+   console.log(playRound())
+   console.log(playRound())
+   console.log(playRound())
+   
 
-    playRound()
 
 }
+
+console.log(game())
